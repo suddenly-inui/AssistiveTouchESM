516de6d51c2ac59238d54db40353c21efa112baf
Yuki Inui <inuiyuki@net43-dhcp94.sfc.keio.ac.jp>
2022-12-15 14:20:12 +0900

    implemented inflate view

--- app/src/main/java/com/android/mirror/assisttouch/service/AssistiveTouchService.java
+++ app/src/main/java/com/android/mirror/assisttouch/service/AssistiveTouchService.java
@@ -4,17 +4,17 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -23,6 +23,8 @@ import android.view.WindowManager;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
+
+import androidx.core.content.res.ResourcesCompat;
 
 import com.android.mirror.assisttouch.R;
 import com.android.mirror.assisttouch.utils.SystemsUtils;
@@ -51,7 +53,6 @@ public class AssistiveTouchService extends Service {
     private PopupWindow mPopupWindow;
     private AlertDialog.Builder mBulider;
     private AlertDialog mAlertDialog;
-    private View mScreenShotView;
 
     private Timer mTimer;
     private Handler mHandler;
@@ -104,7 +105,7 @@ public class AssistiveTouchService extends Service {
         mParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
         mParams.x = mScreenWidth;
         mParams.y = 520;
-        mParams.gravity = Gravity.TOP|Gravity.LEFT;
+        mParams.gravity = Gravity.TOP|Gravity.START;  // LEFT
         mParams.format = PixelFormat.RGBA_8888;
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
         mWindowManager.addView(mAssistiveTouchView, mParams);
@@ -114,65 +115,65 @@ public class AssistiveTouchService extends Service {
                 rawX = event.getRawX();
                 rawY = event.getRawY();
                 switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
+                    case MotionEvent.ACTION_DOWN:  //ボタンタッチ時
                         isMoving = false;
                         break;
-                    case MotionEvent.ACTION_UP:
-                        setAssitiveTouchViewAlign();
+                    case MotionEvent.ACTION_UP:  //ボタン離した時
+                        setAssitiveTouchViewAlign();  // 角に戻る処理
                         break;
-                    case MotionEvent.ACTION_MOVE:
+                    case MotionEvent.ACTION_MOVE:  //ドラッグ時
                         isMoving = true;
                         mParams.x = (int) (rawX - mAssistiveTouchView.getMeasuredWidth() / 2);
                         mParams.y = (int) (rawY - mAssistiveTouchView.getMeasuredHeight() / 2 - mStatusBarHeight);
                         mWindowManager.updateViewLayout(mAssistiveTouchView, mParams);
                 }
-                if (isMoving)
-                    return true;
-                else
-                    return false;
-            }
+                return isMoving;
+            }  // ATに触れた時の処理
         });
-        mAssistiveTouchView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAssistiveTouchView.setAlpha(0);
-                lastAssistiveTouchViewX = mParams.x;
-                lastAssistiveTouchViewY = mParams.y;
-                myAssitiveTouchAnimator(mParams.x,
-                        mScreenWidth / 2 - mAssistiveTouchView.getMeasuredWidth() / 2,
-                        mParams.y,
-                        mScreenHeight / 2 - mAssistiveTouchView.getMeasuredHeight() / 2,
-                        false).start();
+        mAssistiveTouchView.setOnClickListener(v -> {  //AT押下時の処理
+            mAssistiveTouchView.setAlpha(0);
+            lastAssistiveTouchViewX = mParams.x;
+            lastAssistiveTouchViewY = mParams.y;
+            myAssistiveTouchAnimator(mParams.x,
+                    mScreenWidth / 2 - (int)(mScreenWidth*0.75/2),
+                    mParams.y,
+                    mScreenHeight / 2 - (int)(mScreenWidth*0.75/2),
+                    false).start();
 
-                mPopupWindow = new PopupWindow(mInflateAssistiveTouchView,
-                        (int) (mScreenWidth * 0.75), (int) (mScreenWidth * 0.75));
-                mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
-                    @Override
-                    public void onDismiss() {
-                        myAssitiveTouchAnimator(mParams.x, lastAssistiveTouchViewX, mParams.y, lastAssistiveTouchViewY, true).start();
-                        mAssistiveTouchView.setAlpha(1);
-                    }
-                });
-                mPopupWindow.setFocusable(true);
-                mPopupWindow.setTouchable(true);
-                mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
-                mPopupWindow.showAtLocation(mAssistiveTouchView, Gravity.CENTER, 0, 0);
+            mParams.width = (int) (mScreenWidth*0.75);
+            mParams.height = (int) (mScreenWidth*0.75);
+            mWindowManager.updateViewLayout(mAssistiveTouchView, mParams);
 
-                /*mAlertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-                mAlertDialog.show();
-                WindowManager.LayoutParams alertDialogParams = mAlertDialog.getWindow().getAttributes();
-                alertDialogParams.width = (int)(mScreenWidth*0.75);
-                alertDialogParams.height = (int)(mScreenWidth*0.75);
-                alertDialogParams.alpha = 0.85F;
-                mAlertDialog.getWindow().setAttributes(alertDialogParams);
-                mAlertDialog.getWindow().setContentView(mInflateAssistiveTouchView);
-                mAlertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @Override
-                    public void onDismiss(DialogInterface dialog) {
-                        mAssistiveTouchView.setAlpha(1);
-                    }
-                });*/
-            }
+            mPopupWindow = new PopupWindow(mInflateAssistiveTouchView);
+
+            mPopupWindow.setWidth((int) (mScreenWidth * 0.75));  // 横
+            mPopupWindow.setHeight((int) (mScreenWidth * 0.75));  //縦
+
+            mPopupWindow.setOnDismissListener(() -> {
+                mParams.width = (int) (WindowManager.LayoutParams.WRAP_CONTENT);
+                mParams.height = (int) (WindowManager.LayoutParams.WRAP_CONTENT);
+                myAssistiveTouchAnimator(mParams.x, lastAssistiveTouchViewX, mParams.y, lastAssistiveTouchViewY, true).start();
+                mAssistiveTouchView.setAlpha(1);
+            });  //  ポップアップが閉じる処理
+            mPopupWindow.setFocusable(true);
+            mPopupWindow.setTouchable(true);
+            mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+            mPopupWindow.showAtLocation(mAssistiveTouchView, Gravity.CENTER, 0, 0);  //TODO: ここ治す
+
+            /*mAlertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+            mAlertDialog.show();
+            WindowManager.LayoutParams alertDialogParams = mAlertDialog.getWindow().getAttributes();
+            alertDialogParams.width = (int)(mScreenWidth*0.75);
+            alertDialogParams.height = (int)(mScreenWidth*0.75);
+            alertDialogParams.alpha = 0.85F;
+            mAlertDialog.getWindow().setAttributes(alertDialogParams);
+            mAlertDialog.getWindow().setContentView(mInflateAssistiveTouchView);
+            mAlertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    mAssistiveTouchView.setAlpha(1);
+                }
+            });*/
         });
     }
 
@@ -182,17 +183,14 @@ public class AssistiveTouchService extends Service {
 //        ImageView screenshot = (ImageView)mInflateAssistiveTouchView.findViewById(R.id.screenshot);
 //        ImageView home = (ImageView)mInflateAssistiveTouchView.findViewById(R.id.home);
 
-        shutdown.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SystemsUtils.shutDown(AssistiveTouchService.this);
-                mTimer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        mHandler.sendEmptyMessage(0);
-                    }
-                }, 626);
-            }
+        shutdown.setOnClickListener(v -> {
+            SystemsUtils.shutDown(AssistiveTouchService.this);
+            mTimer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    mHandler.sendEmptyMessage(0);
+                }
+            }, 626);
         });
 
 //        home.setOnClickListener(new View.OnClickListener() {
@@ -224,23 +222,20 @@ public class AssistiveTouchService extends Service {
 //                }, 626);
 //            }
 //        });
-    }
+    }  // ポップアップ内の機能押下時の処理
 
-    private ValueAnimator myAssitiveTouchAnimator(final int fromx, final int tox, int fromy, final int toy, final boolean flag){
+    private ValueAnimator myAssistiveTouchAnimator(final int fromx, final int tox, int fromy, final int toy, final boolean flag){
         PropertyValuesHolder p1 = PropertyValuesHolder.ofInt("X", fromx, tox);
         PropertyValuesHolder p2 = PropertyValuesHolder.ofInt("Y", fromy, toy);
         ValueAnimator v1 = ValueAnimator.ofPropertyValuesHolder(p1, p2);
         v1.setDuration(100L);
         v1.setInterpolator(new DecelerateInterpolator());
-        v1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                Integer x = (Integer) animation.getAnimatedValue("X");
-                Integer y = (Integer) animation.getAnimatedValue("Y");
-                mParams.x = x;
-                mParams.y = y;
-                mWindowManager.updateViewLayout(mAssistiveTouchView, mParams);
-            }
+        v1.addUpdateListener(animation -> {
+            Integer x = (Integer) animation.getAnimatedValue("X");
+            Integer y = (Integer) animation.getAnimatedValue("Y");
+            mParams.x = x;
+            mParams.y = y;
+            mWindowManager.updateViewLayout(mAssistiveTouchView, mParams);
         });
         v1.addListener(new AnimatorListenerAdapter() {
             @Override
@@ -253,32 +248,32 @@ public class AssistiveTouchService extends Service {
         return v1;
     }
 
-    /*private ValueAnimator mSceenShotAnimator(){
-        PropertyValuesHolder p1 = PropertyValuesHolder.ofFloat("scaleX", 1, 0);
-        PropertyValuesHolder p2 = PropertyValuesHolder.ofFloat("scaleY", 1, 0);
-        ValueAnimator v1 = ValueAnimator.ofPropertyValuesHolder(p1, p2);
-        v1.setDuration(5000L);
-        v1.setInterpolator(new DecelerateInterpolator());
-        v1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                Float scaleX = (Float) animation.getAnimatedValue("scaleX");
-                Float scaleY = (Float) animation.getAnimatedValue("scaleY");
-                *//*mScreenShotView.setScaleX(scaleX);
-                mScreenShotView.setScaleY(scaleY);*//*
-                *//*WindowManager.LayoutParams lp = mAlertDialog.getWindow().getAttributes();
-                lp.width = (int)(scaleX * mScreenWidth);
-                lp.height = (int)(scaleY * mScreenHeight);
-                mAlertDialog.getWindow().setAttributes(lp);*//*
-                *//*ViewGroup.LayoutParams lp = mScreenShotView.getLayoutParams();
-                lp.width = (int)(scaleX * mScreenWidth);
-                lp.height = (int)(scaleY * mScreenHeight);*//*
-                //mScreenShotView
-                mScreenShotView.setLayoutParams(new FrameLayout.LayoutParams((int)(scaleX * mScreenWidth), (int)(scaleY * mScreenHeight) ));
-            }
-        });
-        return v1;
-    }*/
+//    /*private ValueAnimator mSceenShotAnimator(){
+//        PropertyValuesHolder p1 = PropertyValuesHolder.ofFloat("scaleX", 1, 0);
+//        PropertyValuesHolder p2 = PropertyValuesHolder.ofFloat("scaleY", 1, 0);
+//        ValueAnimator v1 = ValueAnimator.ofPropertyValuesHolder(p1, p2);
+//        v1.setDuration(5000L);
+//        v1.setInterpolator(new DecelerateInterpolator());
+//        v1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator animation) {
+//                Float scaleX = (Float) animation.getAnimatedValue("scaleX");
+//                Float scaleY = (Float) animation.getAnimatedValue("scaleY");
+//                *//*mScreenShotView.setScaleX(scaleX);
+//                mScreenShotView.setScaleY(scaleY);*//*
+//                *//*WindowManager.LayoutParams lp = mAlertDialog.getWindow().getAttributes();
+//                lp.width = (int)(scaleX * mScreenWidth);
+//                lp.height = (int)(scaleY * mScreenHeight);
+//                mAlertDialog.getWindow().setAttributes(lp);*//*
+//                *//*ViewGroup.LayoutParams lp = mScreenShotView.getLayoutParams();
+//                lp.width = (int)(scaleX * mScreenWidth);
+//                lp.height = (int)(scaleY * mScreenHeight);*//*
+//                //mScreenShotView
+//                mScreenShotView.setLayoutParams(new FrameLayout.LayoutParams((int)(scaleX * mScreenWidth), (int)(scaleY * mScreenHeight) ));
+//            }
+//        });
+//        return v1;
+//    }*/
 
     private void setAssitiveTouchViewAlign(){
         int mAssistiveTouchViewWidth = mAssistiveTouchView.getMeasuredWidth();
@@ -296,63 +291,57 @@ public class AssistiveTouchService extends Service {
         if(min == left) mParams.x = 0;
         if(min == right) mParams.x = mScreenWidth - mAssistiveTouchViewWidth;
         if(min == bottom) mParams.y = mScreenHeight - mAssistiveTouchViewHeight;
-        myAssitiveTouchAnimator(lastAssistiveTouchViewX, mParams.x, lastAssistiveTouchViewY, mParams.y, false).start();
-    }
+        myAssistiveTouchAnimator(lastAssistiveTouchViewX, mParams.x, lastAssistiveTouchViewY, mParams.y, false).start();
+    } // ボタン離脱時角に戻る処理
 
-    private void showScreenshot(String name){
-        String path = "/sdcard/Pictures/" + name + ".png";
-        Bitmap bitmap = BitmapFactory.decodeFile(path);
+//    private void showScreenshot(String name){
+//        String path = "/sdcard/Pictures/" + name + ".png";
+//        Bitmap bitmap = BitmapFactory.decodeFile(path);
+//
+//        mScreenShotView = mInflater.inflate(R.layout.screen_shot_show, null);
+//        ImageView imageView = (ImageView)mScreenShotView.findViewById(R.id.screenshot);
+//        imageView.setImageBitmap(bitmap);
+//
+//        mAlertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+//        mAlertDialog.show();
+//        WindowManager.LayoutParams alertDialogParams = mAlertDialog.getWindow().getAttributes();
+//        alertDialogParams.width = mScreenWidth;
+//        alertDialogParams.height = mScreenHeight;
+//        mAlertDialog.getWindow().setAttributes(alertDialogParams);
+//        mAlertDialog.getWindow().setContentView(mScreenShotView);
+//
+//        mTimer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                Message msg = mHandler.obtainMessage();
+//                msg.what = 2;
+//                mHandler.sendMessage(msg);
+//            }
+//        }, 3000);
 
-        mScreenShotView = mInflater.inflate(R.layout.screen_shot_show, null);
-        ImageView imageView = (ImageView)mScreenShotView.findViewById(R.id.screenshot);
-        imageView.setImageBitmap(bitmap);
+        //*mSceenShotAnimator().start();
 
-        mAlertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-        mAlertDialog.show();
-        WindowManager.LayoutParams alertDialogParams = mAlertDialog.getWindow().getAttributes();
-        alertDialogParams.width = mScreenWidth;
-        alertDialogParams.height = mScreenHeight;
-        mAlertDialog.getWindow().setAttributes(alertDialogParams);
-        mAlertDialog.getWindow().setContentView(mScreenShotView);
+        //*ObjectAnimator.ofFloat(mScreenShotView, "translationX", 0, mScreenWidth-mScreenShotView.getX());
+//        ObjectAnimator.ofFloat(mScreenShotView, "translationY", 0, mScreenHeight-mScreenShotView.getY());
+//        ObjectAnimator.ofFloat(mScreenShotView, "scaleX", 1, 0);
+//        ObjectAnimator.ofFloat(mScreenShotView, "scaleY", 1, 0);
 
-        mTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Message msg = mHandler.obtainMessage();
-                msg.what = 2;
-                mHandler.sendMessage(msg);
-            }
-        }, 3000);
-
-        /*mSceenShotAnimator().start();*/
-
-        /*ObjectAnimator.ofFloat(mScreenShotView, "translationX", 0, mScreenWidth-mScreenShotView.getX());
-        ObjectAnimator.ofFloat(mScreenShotView, "translationY", 0, mScreenHeight-mScreenShotView.getY());
-        ObjectAnimator.ofFloat(mScreenShotView, "scaleX", 1, 0);
-        ObjectAnimator.ofFloat(mScreenShotView, "scaleY", 1, 0);*/
-
-        /*mScreenShotView.setPivotX();
-        mScreenShotView.setPivotY();*/
-        /*PropertyValuesHolder p1 = PropertyValuesHolder.ofFloat("X", 0, mScreenWidth);
-        PropertyValuesHolder p2 = PropertyValuesHolder.ofFloat("Y", 0, mScreenHeight/2);
-        PropertyValuesHolder p3 = PropertyValuesHolder.ofFloat("scaleX", 1, 0.5F);
-        PropertyValuesHolder p4 = PropertyValuesHolder.ofFloat("scaleY", 1, 0.5F);
-        ObjectAnimator.ofPropertyValuesHolder(mScreenShotView,p1,p2,p3,p4).setDuration(2000).start();*/
-    }
+        //mScreenShotView.setPivotX();
+        //mScreenShotView.setPivotY();
+        //PropertyValuesHolder p1 = PropertyValuesHolder.ofFloat("X", 0, mScreenWidth);
+//        PropertyValuesHolder p2 = PropertyValuesHolder.ofFloat("Y", 0, mScreenHeight/2);
+//        PropertyValuesHolder p3 = PropertyValuesHolder.ofFloat("scaleX", 1, 0.5F);
+//        PropertyValuesHolder p4 = PropertyValuesHolder.ofFloat("scaleY", 1, 0.5F);
+//        ObjectAnimator.ofPropertyValuesHolder(mScreenShotView,p1,p2,p3,p4).setDuration(2000).start();*/
+//    }
 
     private class MyHandler extends Handler{
         @Override
         public void handleMessage(Message msg) {
-            switch(msg.what){
-                case 1:
-                    showScreenshot((String)msg.obj);
-                    break;
-                case 2:
-                    mAlertDialog.dismiss();
-                default:
-                    mPopupWindow.dismiss();
-                    break;
+            if (msg.what == 2) {
+                mAlertDialog.dismiss();
             }
+            mPopupWindow.dismiss();
             super.handleMessage(msg);
         }
     }
